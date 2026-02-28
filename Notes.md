c9472418060c830280853aaedea91345be0d8fc6
developer-alpinetech <developer@alpinetech-india.com>
2026-02-28 15:01:29 +0530

    first commit

---
+++ Notes.md
@@ -0,0 +1,14 @@
+ <!--  -->
+ we use the .py extension to create a python file
+ we use the module to import the code from another file or use the code of another person with the help of pip
+ pip is a package manager for python which helps us to install the packages and modules
+ we use the # symbol to write the comments in python
+ pip install package_name to install the package pip install pyjokes to install the pyjokes package
+
+ <!-- Module type  -->
+ modules are two types 
+ 1. inbuilt module
+ 2. external module 
+
+ <!-- REPL -->
+ Read Evaluate print loop(it is used to use pytho as an calculator  "just enter the python in terminal perform some operation it give you the calculation")
---
+++ first.py
@@ -0,0 +1,16 @@
+# Learn the module,comments
+#  we use the .py extension to create a python file
+#  we use the module to import the code from another file or use the code of another person with the help of pip
+# pip is a package manager for python which helps us to install the packages and modules
+#  we use the # symbol to write the comments in python
+# pip install package_name to install the package pip install pyjokes to install the pyjokes package
+
+print("Hello python I want to learn python")
+
+import pyjokes
+
+print(pyjokes.get_joke())
+#  we can also use the pyjokes package to get the jokes in different languages and categories
+print(pyjokes.get_joke(language="en", category="all"))
+
+
